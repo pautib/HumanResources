3e2ec0f3b084e501fcb71236cb8645fcc3cab023
pautib <pau.torresibravo@igt.com>
2023-01-30 21:43:00 +0100

    First commit

---
+++ src/main/java/org/pautib/entities/AbstractEntity.java
@@ -0,0 +1,63 @@
+package org.pautib.entities;
+
+import javax.persistence.Version;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class AbstractEntity implements Serializable {
+
+    protected Long id;
+    protected String userEmail;
+
+    @Version
+    protected Long version;
+
+    protected LocalDateTime createdOn;
+
+    protected LocalDateTime updatedOn;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    public LocalDateTime getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(LocalDateTime createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public LocalDateTime getUpdatedOn() {
+        return updatedOn;
+    }
+
+    public void setUpdatedOn(LocalDateTime updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+
+
+
+}
